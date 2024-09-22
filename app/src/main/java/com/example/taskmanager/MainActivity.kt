d221658af08224a8160dd24207b1fb84cb8b878a
Mohamed Amer <mohamedamerjalal@gmail.com>
2024-09-22 21:00:01 +0300

    TaskCompleted

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,10 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    TaskCompletedScreen(
+                    TaskCompletedApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -45,32 +45,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TaskCompletedScreen(
+fun TaskCompletedApp(
     modifier: Modifier = Modifier
 ) {
     Surface(
+        modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(R.drawable.ic_task_completed), contentDescription = null
-            )
-            Text(
-                text = stringResource(R.string.all_task_completed),
-                modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = stringResource(R.string.nice_work), fontSize = 16.sp
-            )
-        }
+        TaskCompletedScreen(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            taskCompleted = stringResource(id = R.string.all_task_completed),
+            workCompleted = stringResource(id = R.string.nice_work)
+        )
     }
+}
+
+@Composable
+fun TaskCompletedScreen(
+    modifier: Modifier = Modifier,
+    painter: Painter,
+    taskCompleted: String,
+    workCompleted: String
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painter, contentDescription = null
+        )
+        TaskCompletedText(
+            text = taskCompleted,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        TaskCompletedText(
+            text = workCompleted, fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun TaskCompletedText(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize: TextUnit = TextUnit.Unspecified
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        fontWeight = fontWeight,
+        fontSize = fontSize,
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -80,7 +109,7 @@ fun TaskManagerThemePreview() {
         Scaffold(
             modifier = Modifier.fillMaxSize()
         ) { innerPadding ->
-            TaskCompletedScreen(
+            TaskCompletedApp(
                 modifier = Modifier.padding(innerPadding)
             )
         }
